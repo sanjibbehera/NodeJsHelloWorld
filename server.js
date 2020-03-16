41d0053213b0bdc770e4f76d70bc589a41196f08
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2020-03-16 15:06:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const app = express()
+const mariadb = require('mariadb');
 const port = 3299
+
+const pool = mariadb.createPool({
+     host: 'mydb.com', 
+     user:'myUser', 
+     password: 'myPassword',
+     connectionLimit: 5
+});
+async function asyncFunction() {
+  let conn;
+  try {
+	conn = await pool.getConnection();
+	const rows = await conn.query("SELECT 1 as val");
+	console.log(rows); //[ {val: 1}, meta: ... ]
+  } catch (err) {
+	throw err;
+  } finally {
+	if (conn) return conn.end();
+  }
+}
 
 app.get('/', (req, res) => res.send('Hello World FROM SANJIB BEHERA LAXMI CHOWK HINJEWADI PUNE!'))
 
